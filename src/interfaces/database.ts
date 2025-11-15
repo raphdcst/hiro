7c7d56df3c8461437f837dcf31530553fe5fdc3c
raphdcst <rdecostaire@gmail.com>
2025-11-15 13:04:09 +0100

    feat(db): expose db status method

--- src/interfaces/database.ts
+++ src/interfaces/database.ts
@@ -1,9 +1,13 @@
 import type { BunSQLDatabase } from 'drizzle-orm/bun-sql'
+import type { Result } from 'neverthrow'
+import type { StatusError } from '#core/errors'
+import type { ServiceStatus } from '#core/service'
 
 export const IDatabaseService = Symbol('IDatabaseService')
 
 export interface IDatabaseService {
 	readonly db: BunSQLDatabase
+	getStatus(): Promise<Result<ServiceStatus, StatusError>>
 }
 
 export type IDatabaseServiceType = IDatabaseService
